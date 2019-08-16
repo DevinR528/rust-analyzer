7b8c20760a4b1c317e5b119737f4ec050499305b
Devin R <devin.ragotzy@gmail.com>
2019-08-16 08:54:47 -0400

    find_block builds block when needed

--- crates/ra_fmt2/src/edit_tree.rs
+++ crates/ra_fmt2/src/edit_tree.rs
@@ -54,7 +54,7 @@ pub(crate) struct Block {
     //indent: some enum?
     element: SyntaxElement,
     text: SmolStr,
-    parent: Box<Option<Block>>,
+    parent: Option<SyntaxNode>,
     range: TextRange,
     prev_whitespace: Option<Whitespace>,
 }
@@ -62,29 +62,10 @@ pub(crate) struct Block {
 // each block will have knowledge of spacing and indent, 
 impl Block {
     pub(crate) fn build_block(element: SyntaxElement) -> Block {
-        // recursivly add to children
-        let first_child = match &element {
-            NodeOrToken::Node(node) => {
-                if let Some(kid) = node.first_child_or_token() {
-                    println!("child or token");
-                    Box::new(Some(Block::build_block(kid)))
-                } else {
-                    Box::new(None)
-                }
-            },
-            NodeOrToken::Token(_) => {
-                Box::new(None)
-            }
-        };
-        let next_sib = if let Some(s) = element.next_sibling_or_token() {
-            Box::new(Some(Block::build_block(s)))
-        } else {
-            Box::new(None)
-        };
         let parent = if let Some(node) = element.parent() {
-            Box::new(Some(Block::build_block(NodeOrToken::Node(node))))
+           Some(node)
         } else {
-            Box::new(None)
+            None
         };
         let range = match &element {
             NodeOrToken::Node(node) => node.text_range(),
@@ -112,34 +93,32 @@ impl Block {
             element,
             text,
             parent,
-            first_child,
-            next_sib,
             range,
             prev_whitespace,
         }
     }
 
-    /// Compare pointers to check if two Blocks are equal.
-    fn compare(&self, other: &Block) -> bool {
-        self as *const _ == other as *const _
-    }
+    // /// Compare pointers to check if two Blocks are equal.
+    // fn compare(&self, other: &Block) -> bool {
+    //     self as *const _ == other as *const _
+    // }
 
-    /// Returns an iterator of parents from current element.
-    fn ancestors(&self) -> Parents<'_> {
-        // what is the best way to do this Rc, Box, Cell with refs?
-        Parents( self.parent.as_ref().as_ref() )
-    }
+    // /// Returns an iterator of parents from current element.
+    // fn ancestors(&self) -> Parents<'_> {
+    //     // what is the best way to do this Rc, Box, Cell with refs?
+    //     Parents( self.parent.as_ref() )
+    // }
 
-    /// Returns an iterator of any sibling nodes and tokens from current element.
-    fn siblings_with_tokens(&self) -> NextSibling<'_> {
-        NextSibling( self.next_sib.as_ref().as_ref() )
-    }
+    // /// Returns an iterator of any sibling nodes and tokens from current element.
+    // fn siblings_with_tokens(&self) -> NextSibling<'_> {
+    //     NextSibling( self.next_sib.as_ref().as_ref() )
+    // }
 
-    /// Walk all the blocks 
-    fn traverse(&self) -> WalkBlocks<'_> {
-        println!("in traverse");
-        WalkBlocks { root: self, next: Some(Branch::Continue(self)) }
-    }
+    // /// Walk all the blocks 
+    // fn traverse(&self) -> WalkBlocks<'_> {
+    //     println!("in traverse");
+    //     WalkBlocks { root: self, next: Some(Branch::Continue(self)) }
+    // }
 
     /// Returns `Whitespace` which has knowledge of whitespace around current token.
     pub(crate) fn get_spacing(&self, tkn: SyntaxToken) -> Option<&Whitespace> {
@@ -153,94 +132,7 @@ impl Block {
     }
 }
 
-pub(crate) struct Parents<'p>(Option<&'p Block>);
-impl<'p> Iterator for Parents<'p> {
-    type Item = &'p Block;
-    fn next(&mut self) -> Option<Self::Item> {
-        if let Some(blk) = self.0.take() {
-            self.0 = blk.parent.as_ref().as_ref();
-            Some(blk)
-        } else {
-            None
-        }
-    }
-}
 
-pub(crate) struct NextSibling<'s>(Option<&'s Block>);
-impl<'s> Iterator for NextSibling<'s> {
-    type Item = &'s Block;
-    fn next(&mut self) -> Option<Self::Item> {
-        if let Some(blk) = self.0.take() {
-            self.0 = blk.next_sib.as_ref().as_ref();
-            Some(blk)
-        } else {
-            None
-        }
-    }
-}
-
-#[derive(Debug, Clone)]
-/// Branch keeps track of where in the tree we are.
-pub(crate) enum Branch<T> {
-    /// At the begining or during child traversal.
-    Continue(T),
-    /// Terminal holds the next sibling in traversal after
-    /// reaching the terminal child.
-    Terminal(T),
-}
-
-impl<T> std::ops::Deref for Branch<T> {
-    type Target = T;
-    fn deref(&self) -> &Self::Target {
-        match self {
-            Branch::Continue(t) => t,
-            Branch::Terminal(t) => t,
-        }
-    }
-}
-
-#[derive(Debug, Clone)]
-pub(crate) struct WalkBlocks<'b> {
-    root: &'b Block,
-    next: Option<Branch<&'b Block>>,
-}
-
-impl<'b> Iterator for WalkBlocks<'b> {
-    type Item = Branch<&'b Block>;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if let Some(branch) = self.next.take() {
-            println!("in WalkBlock");
-            self.next = match branch {
-                Branch::Continue(block) => {
-                    if let Some(child) = block.first_child.as_ref() {
-                        Some(Branch::Continue(child))
-                    } else {
-                        Some(Branch::Terminal(block))
-                    }
-                },
-                Branch::Terminal(block) => {
-                    // we have come back to root done
-                    if block.compare(self.root) {
-                        None
-                    // reached end of children move on to next sibling
-                    } else if let Some(sibling) = block.next_sib.as_ref() {
-                        Some(Branch::Continue(sibling))
-                    // no child or sibling move up level to current Block's parent
-                    } else if let Some(parent) = block.parent.as_ref() {
-                        // we reached the end of branch so current's parent's sibling is next
-                        Some(Branch::Terminal(parent))
-                    } else {
-                        unreachable!("In Branch::Terminal( {:?} )", block)
-                    }
-                }
-            };
-            Some(branch)
-        } else {
-            None
-        }
-    }
-}
 
 #[derive(Debug, Clone)]
 pub(crate) struct EditTree<'n>(&'n SyntaxNode);
@@ -269,3 +161,96 @@ impl<'n> EditTree<'n> {
         
     }
 }
+
+
+
+
+
+// pub(crate) struct Parents<'p>(Option<&'p Block>);
+// impl<'p> Iterator for Parents<'p> {
+//     type Item = &'p Block;
+//     fn next(&mut self) -> Option<Self::Item> {
+//         if let Some(blk) = self.0.take() {
+//             self.0 = blk.parent.as_ref().as_ref();
+//             Some(blk)
+//         } else {
+//             None
+//         }
+//     }
+// }
+
+// pub(crate) struct NextSibling<'s>(Option<&'s Block>);
+// impl<'s> Iterator for NextSibling<'s> {
+//     type Item = &'s Block;
+//     fn next(&mut self) -> Option<Self::Item> {
+//         if let Some(blk) = self.0.take() {
+//             self.0 = blk.next_sib.as_ref().as_ref();
+//             Some(blk)
+//         } else {
+//             None
+//         }
+//     }
+// }
+
+// #[derive(Debug, Clone)]
+// /// Branch keeps track of where in the tree we are.
+// pub(crate) enum Branch<T> {
+//     /// At the begining or during child traversal.
+//     Continue(T),
+//     /// Terminal holds the next sibling in traversal after
+//     /// reaching the terminal child.
+//     Terminal(T),
+// }
+
+// impl<T> std::ops::Deref for Branch<T> {
+//     type Target = T;
+//     fn deref(&self) -> &Self::Target {
+//         match self {
+//             Branch::Continue(t) => t,
+//             Branch::Terminal(t) => t,
+//         }
+//     }
+// }
+
+// #[derive(Debug, Clone)]
+// pub(crate) struct WalkBlocks<'b> {
+//     root: &'b Block,
+//     next: Option<Branch<&'b Block>>,
+// }
+
+// impl<'b> Iterator for WalkBlocks<'b> {
+//     type Item = Branch<&'b Block>;
+
+//     fn next(&mut self) -> Option<Self::Item> {
+//         if let Some(branch) = self.next.take() {
+//             println!("in WalkBlock");
+//             self.next = match branch {
+//                 Branch::Continue(block) => {
+//                     if let Some(child) = block.first_child.as_ref() {
+//                         Some(Branch::Continue(child))
+//                     } else {
+//                         Some(Branch::Terminal(block))
+//                     }
+//                 },
+//                 Branch::Terminal(block) => {
+//                     // we have come back to root done
+//                     if block.compare(self.root) {
+//                         None
+//                     // reached end of children move on to next sibling
+//                     } else if let Some(sibling) = block.next_sib.as_ref() {
+//                         Some(Branch::Continue(sibling))
+//                     // no child or sibling move up level to current Block's parent
+//                     } else if let Some(parent) = block.parent.as_ref() {
+//                         // we reached the end of branch so current's parent's sibling is next
+//                         Some(Branch::Terminal(parent))
+//                     } else {
+//                         unreachable!("In Branch::Terminal( {:?} )", block)
+//                     }
+//                 }
+//             };
+//             Some(branch)
+//         } else {
+//             None
+//         }
+//     }
+// }
